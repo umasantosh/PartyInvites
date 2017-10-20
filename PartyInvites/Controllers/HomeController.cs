7583f86a2a70fd8b8a45311db9d29bef272adafb
Santosh Narayanan <santosh33@gmail.com>
2017-10-20 15:53:27 +0530

    Base project - Added new required files for project setup

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -5,33 +5,73 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
+using PartyInvites.Model;
+using System.Linq;
 
 namespace PartyInvites.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
         }
 
-        public IActionResult About()
+        [HttpPost]
+        public ViewResult Rsvpform(GuestResponse guestResponse)
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            if(ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
         }
 
-        public IActionResult Contact()
+        public ViewResult ListResponses()
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
+        #region Original Code Commented
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "My first ASP.Net Core MVC programming";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+        #endregion
+
     }
 }
---
+++ PartyInvites/Model/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using PartyInvites.Model;
+
+namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
+
